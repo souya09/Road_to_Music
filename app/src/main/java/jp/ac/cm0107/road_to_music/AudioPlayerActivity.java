59f7dd9bb579c0eb32eb939a4620bc82a0027852
YuiYoshino <pikipiki.0809@gmail.com>
2023-11-20 08:56:11 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/cm0107/road_to_music/AudioPlayerActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.cm0107.road_to_music;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+    }
+}
